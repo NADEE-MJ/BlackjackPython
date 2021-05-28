aa8e15e54d9640b077fad09e79b9fc3d0d4896bc
NADEE-MJ <nadeem.maida@gmail.com>
2021-05-28 15:46:50 -0700

    implemented card.py

--- Card.py
+++ Card.py
@@ -4,7 +4,7 @@
 class Card:
     value = 0
     suit = ''
-    
+
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
@@ -15,8 +15,29 @@ class Card:
         if (self.value > 1 and self.value <= 10):
             cardName += self.value
         else:
+            if (self.value == 1):
+                cardName += "Ace"
+            elif (self.value == 11):
+                cardName += "Jack"
+            elif (self.value == 12):
+                cardName += "Queen"
+            elif (self.value == 13):
+                cardName += "King"
+        
+        cardName += " of "
 
-            
+        if (self.suit == 'D'):
+            cardName += "Diamonds"
+        elif (self.suit == 'H'):
+            cardName += "Hearts"
+        elif (self.suit == 'S'):
+            cardName += "Spades"
+        elif (self.suit == 'C'):
+            cardName += "Clubs"
 
 
-    def cardValue():
+    def cardValue(self):
+        if (self.value <= 10):
+            return self.value
+        else:
+            return 10
