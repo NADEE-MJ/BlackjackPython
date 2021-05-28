ba098f3de82dea553df74a45f9ccb0be09bc8f81
NADEE-MJ <nadeem.maida@gmail.com>
2021-05-28 15:34:18 -0700

    added class files, will impliment code later

---
+++ Card.py
@@ -0,0 +1,22 @@
+
+
+
+class Card:
+    value = 0
+    suit = ''
+    
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def displayCardName(self):
+        cardName = "["
+
+        if (self.value > 1 and self.value <= 10):
+            cardName += self.value
+        else:
+
+            
+
+
+    def cardValue():
