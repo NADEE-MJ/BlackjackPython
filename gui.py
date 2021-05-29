f28cf2a099db1a09903dc5d921c4b3ad3d3bb5f9
NADEE-MJ <nadeem.maida@gmail.com>
2021-05-28 19:08:36 -0700

    replaced card jpgs with pngs

--- gui.py
+++ gui.py
@@ -13,6 +13,8 @@ while running:
             running = False
 
     screen.fill((0, 255, 0))
-
-
+    image = pygame.image.load("Cards/2C.jpg")
+    screen.blit(image, (50, 50))
+    pygame.display.flip()
+    
 pygame.quit()
